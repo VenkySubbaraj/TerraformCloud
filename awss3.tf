aa48843822f063906aac6bfd0d78893820ab61a5
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2022-03-05 17:11:29 +0000

    changes

--- awss3.tf
+++ awss3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "dockercontainer" { 
-	bucket="dockercontainer1"
-	acl="public-read"
-	
-	tags = {
-		Name = "DockerContainer2"
-	}
+resource "aws_s3_bucket" "dockercontainer" {
+ bucket = "dockercontainer1"
+ acl    = "public-read"
+
+ tags = {
+   Name = "DockerContainer2"
+ }
 }
