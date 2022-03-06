8e638994ff6101313378030508b7e4d0dfa532a3
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-03-06 11:14:42 +0530

    Update awss3.tf

--- awss3.tf
+++ awss3.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "dockercontainer1" {
 }
 
 resource "aws_s3_bucket_acl" "dockercontainer_acl" {
- bucket = aws_s3_bucket.dockercontainer.id
+ bucket = aws_s3_bucket.dockercontainer1.id
  acl = "private"
 }
