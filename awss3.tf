b408399dfaa293cde9f3acce9a62e3960255c989
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2022-03-07 10:07:00 +0000

    changes

--- awss3.tf
+++ awss3.tf
@@ -1,15 +1,15 @@
 
-resource "aws_s3_bucket" "dockercontainer" {
- bucket = var.s3_bucket_name
- tags = {
-   Name = var.tag_name
- }
-}
+#resource "aws_s3_bucket" "dockercontainer" {
+# bucket = var.s3_bucket_name
+# tags = {
+#   Name = var.tag_name
+# }
+#}
 
-resource "aws_s3_bucket_acl" "dockercontainer_acl" {
- bucket = aws_s3_bucket.dockercontainer.id
- acl = var.acl_value
-}
+#resource "aws_s3_bucket_acl" "dockercontainer_acl" {
+# bucket = aws_s3_bucket.dockercontainer.id
+# acl = var.acl_value
+#}
 
 resource "aws_cloudwatch_event_rule" "console" {
   name        = "capture-aws-sign-in"
