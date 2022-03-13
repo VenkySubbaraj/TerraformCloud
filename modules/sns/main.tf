d2d088c472e04c952a33ab98f44b4f6ed396f898
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2022-03-13 15:44:37 +0000

    changes

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -6,7 +6,7 @@ resource "aws_sqs_queue" "queue_service" {
   name = "aws_queue_service"
 }
 
-resource "aws_sns_topic_subrscription" "sub-topics" {
+resource "aws_sns_topic_subscription" "sub-topics" {
   topic_arn = aws_sns_topic.topic-update.arn
   protocol = "email"
   endpoint = aws_sqs_queue.queue_service.arn
