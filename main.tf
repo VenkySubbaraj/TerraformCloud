a991cd01c8572061340d7fd961090aaad339eeb4
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2022-03-24 11:02:05 +0000

    changes

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@
 #  source = "./modules/kms_key"
 #}
 
-#module "App_runner" {
-# source = "./modules/App_runner"
-#}
+module "App_runner" {
+ source = "./modules/App_runner"
+}
 
 module "ECR_PublicRepo" {
  source = "./modules/ECR_PublicRepo"
