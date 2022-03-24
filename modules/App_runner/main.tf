cd44cf6e5bfce4622e49aab00bc576040c247c04
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2022-03-24 11:36:35 +0000

    changes

--- modules/App_runner/main.tf
+++ modules/App_runner/main.tf
@@ -49,7 +49,7 @@ resource "aws_apprunner_service" "ECR-repo" {
      image_repository_type = "ECR_PUBLIC"
     }
    }
-
+     auto_deployments_enabled = false 
  tags = {
    Name = "example-apprunner-service"
 }
