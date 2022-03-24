37521a439eb633c13f6ce42446f0087bdd2731ad
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2022-03-24 10:55:08 +0000

    changes

--- modules/App_runner/main.tf
+++ modules/App_runner/main.tf
@@ -7,34 +7,34 @@ resource "aws_apprunner_connection" "apprunner_connection" {
   }
 }
 
-resource "aws_apprunner_service" "app_runner" {
-  service_name = "app_runner"
+#resource "aws_apprunner_service" "app_runner" {
+#  service_name = "app_runner"
 
-  source_configuration {
-    authentication_configuration {
-      connection_arn = aws_apprunner_connection.apprunner_connection.arn
-    }
-    code_repository {
-      code_configuration {
-        code_configuration_values {
-          build_command = "python pip install -r requirements.txt"
-          port          = "8000"
-          runtime       = "PYTHON_3"
-          start_command = "python app.py"
-        }
-        configuration_source = "API"
-      }
-      repository_url = "https://github.com/VenkySubbaraj/TerraformCloud/"
-      source_code_version {
-        type  = "BRANCH"
-        value = "master"
-      }
-    }
-  } 
+#  source_configuration {
+#    authentication_configuration {
+#      connection_arn = aws_apprunner_connection.apprunner_connection.arn
+#    }
+#    code_repository {
+#      code_configuration {
+#        code_configuration_values {
+#          build_command = "python pip install -r requirements.txt"
+#          port          = "8000"
+#          runtime       = "PYTHON_3"
+#          start_command = "python app.py"
+#        }
+#        configuration_source = "API"
+#      }
+#      repository_url = "public.ecr.aws/e3n8w8r2/publicrepo"
+      #source_code_version {
+      #  type  = "BRANCH"
+      #  value = "master"
+      #}
+#    }
+#  } 
 
-  tags = {
-    Name = "apprunner-service"
-  }
+#  tags = {
+#    Name = "apprunner-service"
+#  }
 }
 
 output "arnvalue" {
