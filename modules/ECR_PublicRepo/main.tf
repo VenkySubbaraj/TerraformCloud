5af0a3559f97b2cd375dd5d2e0f6a6c90284a4cf
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-03-24 20:40:49 +0530

    Update main.tf

--- modules/ECR_PublicRepo/main.tf
+++ modules/ECR_PublicRepo/main.tf
@@ -1,17 +1,17 @@
-provider "aws" {
- alias = "us_east_1"
- region = "us-east-1"
-}
+# provider "aws" {
+#  alias = "us_east_1"
+#  region = "us-east-1"
+# }
 
-resource "aws_ecrpublic_repository" "public_repo" {
- provider = aws.us_east_1
- repository_name = "publicrepo"
+# resource "aws_ecrpublic_repository" "public_repo" {
+#  provider = aws.us_east_1
+#  repository_name = "publicrepo"
 
-catalog_data {
- about_text = "About_text"
- architectures = ["ARM"]
- description = "Description"
- operating_systems = ["linux"]
- usage_text = "Usage_Text"
-}
-}
+# catalog_data {
+#  about_text = "About_text"
+#  architectures = ["ARM"]
+#  description = "Description"
+#  operating_systems = ["linux"]
+#  usage_text = "Usage_Text"
+# }
+# }
