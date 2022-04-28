bfaf68f17ab30910eaff7116dc7284738f092b7e
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-04-28 23:38:54 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,21 +1,21 @@
-resource "aws_s3_bucket" "dockercontainer" { 
-	bucket="dockercontainer2"
-	#acl="public-read"
+# resource "aws_s3_bucket" "dockercontainer" { 
+# 	bucket="dockercontainer2"
+# 	#acl="public-read"
 	
-	tags = {
-		Name = "DockerContainer2"
-	}
-# 	lifecycle_rule {
-#             id = "log"
-# 	    enabled = true
-# 	expiration {
-# 	    days = 90
+# 	tags = {
+# 		Name = "DockerContainer2"
 # 	}
-# 	}
-}
+# # 	lifecycle_rule {
+# #             id = "log"
+# # 	    enabled = true
+# # 	expiration {
+# # 	    days = 90
+# # 	}
+# # 	}
+# }
 
 resource "aws_s3_bucket_lifecycle_configuration" "example" {
-  bucket = dockercontainer2
+  bucket = "dockercontainer2"
   rule {
     id = "rule-1"
 
