1ff165d95d89a57233c479fd5528d6e1b7cfa434
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-04-28 23:28:22 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -5,7 +5,11 @@ resource "aws_s3_bucket" "dockercontainer" {
 	tags = {
 		Name = "DockerContainer2"
 	}
+	lifecycle_rule {
+            id = "log"
+	    enabled = true
 	expiration {
 	    days = 90
+	}
 	}
 }
